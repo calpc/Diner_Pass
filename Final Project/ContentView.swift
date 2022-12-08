cb265c1c88b3fe282eb380911d7cca8c5aab2352
Priscilla Chen <pchencal@berkeley.edu>
2022-12-08 13:55:31 -0800

    added bottom

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -8,41 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, world!")
-//            .padding()
-//    }
-    
-    var body: some View {
-        ZStack {
-            Color.green.ignoresSafeArea()
-            
-            VStack(alignment: .leading) {
-                Spacer()
-                Spacer()
-                Text("Discovery").font(.system(size: 35)).padding(.horizontal).foregroundColor(.white)
-//                Image("front_page").resizable()
-//
-//                Image("restaurant").resizable().scaledToFit().padding()
-                
-                Image("card").resizable()
-                
-//                NavigationView {
-//
-//                }.navigationTitle("Shopping List")
-            }
-        }
-        
-        Image("option").resizable().scaledToFit().padding()
-        
-        Spacer()
-        
-        Image("main_menu").resizable().scaledToFit()
-    }
+   @State var friendsPage = false
+   @State var profilePage = false
+   @State var homePage = false
+
+   var body: some View {
+      ZStack {
+         Image("Discover").resizable().scaledToFit().aspectRatio(contentMode: .fill)
+
+      ZStack {
+         Button("  ") {
+            friendsPage.toggle()
+            homePage = false
+            profilePage = false
+         }.offset(y: 410)
+         Button("  ") {
+            profilePage.toggle()
+            friendsPage = false
+            homePage = false
+         }.offset(x: 150, y: 410)
+         Button("  ") {
+            homePage.toggle()
+            friendsPage = false
+            profilePage = false
+         }.offset(x: -150, y: 410)
+         if homePage {
+            Image("Discover").resizable().scaledToFit().aspectRatio(contentMode: .fill).offset(y: -6)
+            Button("  ") {
+               friendsPage.toggle()
+               homePage = false
+               profilePage = false
+            }.offset(y: 410)
+            Button("  ") {
+               profilePage.toggle()
+               friendsPage = false
+               homePage = false
+            }.offset(x: 150, y: 410)
+         }
+         if friendsPage {
+            Image("Friends List").resizable().scaledToFit().aspectRatio(contentMode: .fill).offset(y: -6)
+            Button("  ") {
+               homePage.toggle()
+               friendsPage = false
+               profilePage = false
+            }.offset(x: -150, y: 410)
+            Button("  ") {
+               profilePage.toggle()
+               friendsPage = false
+               homePage = false
+            }.offset(x: 150, y: 410)
+         }
+         if profilePage {
+            Image("Profile").resizable().scaledToFit().aspectRatio(contentMode: .fill).offset(y: -6)
+            Button("  ") {
+               friendsPage.toggle()
+               homePage = false
+               profilePage = false
+            }.offset(y: 410)
+            Button("  ") {
+               homePage.toggle()
+               friendsPage = false
+               profilePage = false
+            }.offset(x: -150, y: 410)
+         }
+      }
+   }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
